24d0667bcb50b1b5b2026d102b0ae9cc51ff77e3
ISCCarlos <carlos.chavez.arias@gmail.com>
2022-05-20 20:33:17 -0500

    N/A: Finish contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,31 @@
-import { Paper, Typography, Button, Input, Box } from "@mui/material";
-import React from "react";
+import {
+  Paper,
+  Typography,
+  Button,
+  Box,
+  TextField,
+  Snackbar,
+} from "@mui/material";
+import React, { useState } from "react";
 import MailIcon from "@mui/icons-material/Mail";
 import { useTranslation } from "react-i18next";
 import { saveAs } from "file-saver";
+import emailjs from "emailjs-com";
+import MuiAlert from "@mui/material/Alert";
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 const Contact = () => {
   const [translations, i18n] = useTranslation("global");
+  const [open, setOpen] = useState(false);
+  const [alertSeverity, setAlertSeverity] = useState("warning");
+  const [message, setMessage] = useState("");
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const getCV = () => {
     const language = i18n.language;
@@ -13,6 +33,62 @@ const Contact = () => {
     const file = "../../assets/docs/" + language + "_CV.pdf";
 
     saveAs(file, "CV.pdf");
+  };
+
+  const showAlert = () => {
+    setOpen(true);
+  };
+
+  const serviceID = "service_uce0tye";
+  const templateID = "template_92kulkn";
+  const userID = "kfgV-GW04lXMbcHbX";
+
+  const sendEmail = () => {
+    let error = false;
+    const fromName = document.getElementById("name").value;
+    const phone = document.getElementById("phone").value;
+    const replyTo = document.getElementById("mail").value;
+
+    if (fromName === "") {
+      error = true;
+      setAlertSeverity("warning");
+      setMessage("common.invalidName");
+      showAlert();
+    }
+
+    if (phone === "") {
+      error = true;
+      setAlertSeverity("warning");
+      setMessage("common.invalidPhone");
+      showAlert();
+    }
+
+    if (replyTo === "") {
+      error = true;
+      setAlertSeverity("warning");
+      setMessage("common.invalidMail");
+      showAlert();
+    }
+
+    if (!error) {
+      emailjs
+        .send(
+          serviceID,
+          templateID,
+          {
+            from_name: fromName,
+            phone: phone,
+            reply_to: replyTo,
+          },
+          userID
+        )
+        .then(() => {
+          error = false;
+          setAlertSeverity("success");
+          setMessage("common.successMail");
+          showAlert();
+        });
+    }
   };
 
   return (
@@ -25,30 +101,35 @@ const Contact = () => {
       }}
       id="contact"
     >
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert
+          onClose={handleClose}
+          severity={alertSeverity}
+          sx={{ width: "100%" }}
+        >
+          {translations(message)}
+        </Alert>
+      </Snackbar>
       <Typography variant="h3" sx={{ m: 3 }}>
         <MailIcon /> {translations("contact.title")}
       </Typography>
       <Box
         sx={{ width: "90%", maxWidth: "90%", ml: "5%", textAlign: "center" }}
       >
-        <Input
-          type="text"
-          name="name"
+        <TextField
           id="name"
-          placeholder={translations("contact.name")}
+          label={translations("contact.name")}
           sx={{ m: 1 }}
         />
-        <Input
-          type="email"
-          name="mail"
+        <TextField
           id="mail"
-          placeholder={translations("contact.email")}
+          type="email"
+          label={translations("contact.email")}
           sx={{ m: 1 }}
         />
-        <Input
-          type="tel"
-          name="phone"
+        <TextField
           id="phone"
+          type="tel"
           placeholder={translations("contact.phone")}
           sx={{ m: 1 }}
         />
@@ -56,7 +137,12 @@ const Contact = () => {
       <Box
         sx={{ width: "90%", maxWidth: "90%", ml: "5%", textAlign: "center" }}
       >
-        <Button variant="contained" sx={{ m: 1 }}>
+        <Button
+          type="submit"
+          variant="contained"
+          sx={{ m: 1 }}
+          onClick={sendEmail}
+        >
           {translations("contact.submit")}
         </Button>
         <Button variant="outlined" sx={{ m: 1 }} onClick={getCV}>
